9b593b6a5a7fdf75a8617c9564d830bc1a4d83a2
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-15 15:27:30 +0530

    feat:Extend max method to print

--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -9,26 +9,20 @@ namespace FindMaximumNumber
             //Welcome Message
             Console.WriteLine("Welcome to Maximum Number");
 
+            //Array With Different Data Types
             int[] intArray = { 34, 23, 12, 36 };
             double[] doubleArray = { 12.5, 45.65, 987.45, 658.78 };
             string[] stringArray = { "Apple", "Peach", "Banana" };
 
-            // Passing integer input with max number at first place
+            // Objects 
             ExtendGenericMaximum<int> extendGenericMaximum = new ExtendGenericMaximum<int>(intArray);
-            int outputInt = extendGenericMaximum.MaxMethod();
-
             ExtendGenericMaximum<double> extendGenericMaximum1 = new ExtendGenericMaximum<double>(doubleArray);
-            double outputDouble = extendGenericMaximum1.MaxMethod();
-
             ExtendGenericMaximum<string> extendGenericMaximum2 = new ExtendGenericMaximum<string>(stringArray);
-            string outputString = extendGenericMaximum2.MaxMethod();
-
 
             //Display Output
-            Console.WriteLine("Integer Max : " + outputInt);
-            Console.WriteLine("Float Max : " + outputDouble);
-            Console.WriteLine("String Max : " + outputString);
-
+            extendGenericMaximum.PrintMaxValue();
+            extendGenericMaximum1.PrintMaxValue();
+            extendGenericMaximum2.PrintMaxValue();
         }
     }
 }
