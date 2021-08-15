a87da4b18a30ba0fee7753e436687bb1aa9a7d6f
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-15 15:16:52 +0530

    feat:Float Maximum

--- TestMaximum/Program.cs
+++ TestMaximum/Program.cs
@@ -11,10 +11,18 @@ namespace FindMaximumNumber
 
             //Calling Class to call a Function
             MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
-            int outputInteger = maximumNumberCheck.MaximumIngerNumber(11, 22, 33);
 
-            //Display Output
-            Console.WriteLine("Maximum Number of Integer : " + outputInteger);
+            //Maximum Number At First Place
+            double outputFloat1 = maximumNumberCheck.MaximumFloatNumber(14.1, 2.82, 7.33);
+            Console.WriteLine("Maximum Number of Float at First Place : " + outputFloat1);
+
+            //Maximum Number At Second Place
+            double outputFloat2 = maximumNumberCheck.MaximumFloatNumber(4.1, 28.2, 12.8);
+            Console.WriteLine("Maximum Number of Integer at Second Place : " + outputFloat2);
+
+            //Maximum Number At Third Place
+            double outputFloat3 = maximumNumberCheck.MaximumFloatNumber(75.9, 68.47, 77.3);
+            Console.WriteLine("Maximum Number of Integer at Third Place : " + outputFloat3);
         }
     }
 }
